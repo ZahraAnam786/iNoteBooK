cdfeeb9095439cb1fb9b4418694b25a6ba4a5609
ZahraAnam786 <anamwork786@gmail.com>
2024-07-30 17:18:05 +0500

    Save Data in Database

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
-import { type } from "@testing-library/user-event/dist/type";
-import mongoose from "mongoose";
+
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique,
+    unique: true,
   },
   password: {
     type: String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,21 @@
 
 const express = require('express');
-
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) =>{
+
+//Creating a user using POST: "/api/auth/", Doesn't require Auth
+router.post('/', async (req, res) =>{
    console.log(req.body);
-
-   res.json("fsadf")
+   const user = User(req.body);
+   try {
+    await user.save();
+    res.status(201).send(user); // Respond with the created user and status code 201
+  } catch (err) {
+    console.error('Error saving user:', err.message);
+    res.status(500).send({ error: 'Error saving user' }); // Send an error response with status code 500
+  }
 });
 
 
