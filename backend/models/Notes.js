651057e710d88ce5a60b97a366b84c17295cc25b
ZahraAnam786 <anamwork786@gmail.com>
2024-07-30 16:10:30 +0500

    make small api routes

---
+++ backend/models/Notes.js
@@ -0,0 +1,26 @@
+import { type } from "@testing-library/user-event/dist/type";
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  description: {
+    type: String,
+    required: true
+  },
+  tag: {
+    type: String,
+    default: "General"
+  },
+  date:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+module.exports = mongoose.model("notes", NotesSchema);
