15c4f1c96fbd7337faf2968338be28fc72cba22e
ZahraAnam786 <anamwork786@gmail.com>
2024-10-22 17:40:07 +0500

    Add some files

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddNote from './AddNote';
+
+const Home = () => {
+  return (
+    <div className='container' style={{ marginTop: '7rem' }}>
+     <AddNote/>
+    </div>
+  )
+}
+
+export default Home
+
